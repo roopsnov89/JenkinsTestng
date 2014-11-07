9fe31e9ac0466477dc9b7a8e0c696cd88c5990e2
Roopam <roopsnov89@gmail.com>
2014-11-07 16:27:54 +0530

    mwewe

--- src/test/java/com/company/testclasses/Branch1.java
+++ src/test/java/com/company/testclasses/Branch1.java
@@ -4,19 +4,20 @@ package com.company.testclasses;
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Parameters;
+
 import org.testng.annotations.Test;
 
 public class Branch1 {
 	
 	WebDriver d;
 	
-	@Parameters({"name","path"})
+	
     @Test
-    public void bn(String name,String path){
-		System.setProperty(name,path);
-		d = new ChromeDriver();
+    public void bn(){
+		
+			
+		
+		
 		System.out.println("hello Branch1");
 	}
  
--- src/test/java/com/company/testclasses/GoogleTest.java
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -1,11 +1,18 @@
 package com.company.testclasses;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
 	
-	
+	@Parameters({"browser"})
     @Test
-    public void Test1(){
-		System.out.println("hello first Test");
+    public void Test1(String browser){
+		if (browser.equalsIgnoreCase("chrome")){
+		System.out.println("hello its chrome");
+		}
+		if (browser.equals("firefox")){
+			System.out.println("hello its firefox");
+		}
 	}
+	
 }
