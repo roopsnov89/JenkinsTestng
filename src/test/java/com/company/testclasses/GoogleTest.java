064cd07c6289071480c3811108d50351a2726499
Roopam <roopsnov89@gmail.com>
2014-11-07 15:17:01 +0530

    mewe

--- src/test/java/com/company/testclasses/GoogleTest.java
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -1,16 +1,43 @@
 package com.company.testclasses;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
+
+	private WebDriver d;
 	
-	@Test
-	public void New(){
-		System.out.println("hello world");
+	@BeforeTest
+	public void before(){
+		
+		
+		d = new FirefoxDriver();
+		 d.manage().window().maximize();
+		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
-@Test
-public void Test(){
-	System.out.println("new test method");
-}
+	
+	
+	@Parameters({"Browser1"})
+	@Test
+    public void Test() throws InterruptedException{
+		System.out.println("new test method");
+		d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
+
+		d.findElement(By.id("gbqfq")).sendKeys("Manage");
+		d.findElement(By.id("gbqfb")).click();
+		Thread.sleep(2000);
+	}
+	@AfterTest
+	 public void at(){
+		 d.quit();
+	 }
 
 }
