22602704997c82b00aa86d4e2ba23327c9356823
Roopam <roopsnov89@gmail.com>
2014-11-07 16:11:04 +0530

    mkwe

--- src/test/java/com/company/testclasses/Branch1.java
+++ src/test/java/com/company/testclasses/Branch1.java
@@ -3,14 +3,20 @@ package com.company.testclasses;
 
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Branch1 {
 	
-	@Parameters({"Browser2"})
+	WebDriver d;
+	
+	@Parameters({"name","path"})
     @Test
-    public void Test1(){
+    public void bn(String name,String path){
+		System.setProperty(name,path);
+		d = new ChromeDriver();
 		System.out.println("hello Branch1");
 	}
  
--- src/test/java/com/company/testclasses/GoogleTest.java
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -1,13 +1,9 @@
 package com.company.testclasses;
-
-
-
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
 	
-	@Parameters({"Browser1"})
+	
     @Test
     public void Test1(){
 		System.out.println("hello first Test");
