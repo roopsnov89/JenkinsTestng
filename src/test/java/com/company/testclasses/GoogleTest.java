12fad6e28c0fc7110d619bb4fd7592f8042c04ea
Roopam <roopsnov89@gmail.com>
2014-11-07 20:02:46 +0530

    hvg

--- src/test/java/com/company/testclasses/GoogleTest.java
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -14,41 +13,34 @@ public class GoogleTest {
 	private WebDriver d;
 
 	@Parameters({"browser"})
-	@BeforeTest
+	@Test
 	public void Test1(String browser) throws InterruptedException{
-		if (browser.equalsIgnoreCase("chrome")){
+		if (browser.equals("chrome")){
 			System.out.println("hello its chrome");
-			chrome();
+			System.setProperty("webdriver.chrome.driver", "F:/Selenium/chromedriver_win32/chromedriver.exe");
+			d = new ChromeDriver();
+			d.manage().window().maximize();
+			d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
+			d.findElement(By.id("gbqfq")).sendKeys("selenium");
+			d.findElement(By.id("gbqfb")).click();
+			Thread.sleep(2000);
 		}
 
 		if (browser.equals("firefox")){
 			System.out.println("hello its firefox");
-			firefox();
+			d = new FirefoxDriver();
+			d.manage().window().maximize();
+			d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
+			d.findElement(By.id("gbqfq")).sendKeys("selenium");
+			d.findElement(By.id("gbqfb")).click();
+			Thread.sleep(2000);
 
 		}
 	}
-	@Test
-	public void firefox() throws InterruptedException{
-	    d = new FirefoxDriver();
-		d.manage().window().maximize();
-		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
-		d.findElement(By.id("gbqfq")).sendKeys("selenium");
-		d.findElement(By.id("gbqfb")).click();
-		Thread.sleep(2000);
-	}
-	@Test
-	public void chrome() throws InterruptedException{
-		System.setProperty("webdriver.chrome.driver", "F:/Selenium/chromedriver_win32/chromedriver.exe");
-		d = new ChromeDriver();
-		d.manage().window().maximize();
-		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
-		d.findElement(By.id("gbqfq")).sendKeys("selenium");
-		d.findElement(By.id("gbqfb")).click();
-		Thread.sleep(2000);
-	}
 	
+
 	 @AfterTest
 	 public void at(){
 		 d.quit();
