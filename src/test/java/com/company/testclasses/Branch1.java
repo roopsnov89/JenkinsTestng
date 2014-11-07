eb718432bbe8c80188a53618921b9b9928195745
Roopam <roopsnov89@gmail.com>
2014-11-07 15:58:54 +0530

    newre

--- src/test/java/com/company/testclasses/Branch1.java
+++ src/test/java/com/company/testclasses/Branch1.java
@@ -1,41 +1,21 @@
 package com.company.testclasses;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+
+
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Branch1 {
 	
-	private WebDriver d;
-	@BeforeTest
-	public void bt(){
-		
-		System.setProperty("webdriver.chrome.driver", "F:/Selenium/chromedriver_win32/chromedriver.exe");
-		d = new ChromeDriver();
-		 d.manage().window().maximize();
-		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
 	@Parameters({"Browser2"})
- @Test
- public void t() throws InterruptedException{
-	 d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
-
-		d.findElement(By.id("gbqfq")).sendKeys("selenium");
-		d.findElement(By.id("gbqfb")).click();
-		Thread.sleep(2000);
-	 
-	 
- }
+    @Test
+    public void Test1(){
+		System.out.println("hello Branch1");
+	}
  
- @AfterTest
- public void at(){
-	 d.quit();
- }
+	 
+	 
+ 
 	
 }
--- src/test/java/com/company/testclasses/GoogleTest.java
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -1,43 +1,15 @@
 package com.company.testclasses;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
-
-	private WebDriver d;
-	
-	@BeforeTest
-	public void before(){
-		
-		
-		d = new FirefoxDriver();
-		 d.manage().window().maximize();
-		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-	
 	
 	@Parameters({"Browser1"})
-	@Test
-    public void Test() throws InterruptedException{
-		System.out.println("new test method");
-		d.get("https://www.google.co.in//?gfe_rd=cr&ei=DVI_VN3kEuTA8gftlIHYCQ&gws_rd=ssl");
-
-		d.findElement(By.id("gbqfq")).sendKeys("Manage");
-		d.findElement(By.id("gbqfb")).click();
-		Thread.sleep(2000);
+    @Test
+    public void Test1(){
+		System.out.println("hello first Test");
 	}
-	@AfterTest
-	 public void at(){
-		 d.quit();
-	 }
-
 }
