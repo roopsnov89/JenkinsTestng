5b269725e74a0e2a0d8f1836c3782dd8f143657b
Roopam <roopsnov89@gmail.com>
2014-11-07 13:45:44 +0530

    newTestng

---
+++ src/test/java/com/company/testclasses/Branch1.java
@@ -0,0 +1,12 @@
+package com.company.testclasses;
+
+import org.testng.annotations.Test;
+
+public class Branch1 {
+	@Test
+	public void Branch(){
+		System.out.println("Hello this is branch1");
+	}
+
+	
+}
---
+++ src/test/java/com/company/testclasses/GoogleTest.java
@@ -0,0 +1,16 @@
+package com.company.testclasses;
+
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	
+	@Test
+	public void New(){
+		System.out.println("hello world");
+	}
+@Test
+public void Test(){
+	System.out.println("new test method");
+}
+
+}
